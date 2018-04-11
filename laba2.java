79c24d26feac0f64068406772eb233efc3a9610c
ArtemRyabikov <36229388+ArtemRyabikov@users.noreply.github.com>
2018-04-11 13:48:58 +0300

    Update laba2.java

--- laba2.java
+++ laba2.java
@@ -4,20 +4,13 @@
 public class laba2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int x = 2;
+        double a = 0.75;
+        double b = 1.1;
+        double c = 0.4;
         System.out.print("Введіть 0 для обчислення заданих чисел або 1 для вводу з клавіатури : ");
         int k = in.nextInt();
-        int x=0;
-        double a=0;
-        double b=0;
-        double c=0;
-        /**Введення заданих чисел*/
-        if ( k == 0) {
-            x = 2;
-            a = 0.75;
-            b = 1.1;
-            c = 0.4;
-        }
-       else if( k == 1)
+        if( k == 1)
         {
             System.out.print("Введите знаение х: ");
             x = in.nextInt();
@@ -27,10 +20,6 @@ public class laba2 {
             b = in.nextDouble();
             System.out.print("Введите значения c: ");
             c = in.nextDouble();
-        }
-         else {
-            System.out.println("Ви ввели не вірне значення. Будь-ласка перевірте його ще раз або спробуйте виконати дію пізніше.");
-            System.out.println("You have entered a wrong value. Please check it again or try an action later.");
         }
         System.out.println(Math.pow(Math.sin((a * x - 1) / (Math.pow(b, 3) * Math.sqrt(Math.pow(x, 2) - c))), -1) + Math.pow(Math.tan(b / (a * Math.pow(x, 2) + c)), -1));
     }
